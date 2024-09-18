8f83e3331e0122dc318a82fe2511e9806237b969
ParzivalPerhaps <HaydenNinja@hotmail.com>
2024-09-17 19:39:18 -0500

    first commit

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,97 @@
+import { Outlet } from "react-router-dom";
+import { PersonalLogo } from "./Logo";
+
+export const Navbar = () => (
+    <div className="App">
+
+    
+    <div className="Navbar">
+        
+    <a href="/">
+        <div className="NavbarBrand">
+        <PersonalLogo height='67' width='69'/>
+        </div>
+    </a>
+    
+    <div className="NavbarItem">
+      {
+        window.location.href.substring(window.location.href.lastIndexOf("/"), window.location.href.length) === "/" ?
+            <p className="TextHeavy" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
+              HOME
+            </p>
+
+          :
+          <a href="/">
+              <p className="TextHeavy">
+              HOME
+              </p>
+          </a>
+      }
+          
+      </div>
+    <div className="NavbarItem">
+          <a href="/blogs">
+              <p className="TextHeavy">
+                BLOG
+              </p>
+          </a>
+      </div>
+
+      <div className="NavbarItem">
+          <a href="/contact">
+              <p className="TextHeavy">
+                CONTACT
+              </p>
+          </a>
+      </div>
+
+      {
+      window.location.href.substring(window.location.href.lastIndexOf("/"), window.location.href.length) === "/" && <>
+      <div className="NavbarItem" onClick={() => window.scrollTo({ top: 860, behavior: "smooth" })}>
+              <p className="TextHeavy" style={{backgroundColor:'#2E3744', color:'#F1F1F1', padding:'5px', borderRadius:'4px'}}>
+                  ABOUT ISM
+              </p>
+      </div>
+    
+      <div className="NavbarItem" onClick={() => window.scrollTo({ top: 1350, behavior: "smooth" })}>
+      <p className="TextHeavy" style={{backgroundColor:'#2E3744', color:'#F1F1F1', padding:'5px', borderRadius:'4px'}}>
+                  PROJECTS
+              </p>
+        </div>
+        </>
+      }
+
+      {
+      window.location.href.includes("scifi-ai-art-gallery") && <>
+      <div className="NavbarItem" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
+              <p className="TextHeavy" style={{backgroundColor:'#2E3744', color:'#F1F1F1', padding:'5px', borderRadius:'4px'}}>
+                  LEARNING
+              </p>
+      </div>
+    
+      <div className="NavbarItem" onClick={() => window.scrollTo({ top: ((document.getElementById('highlights')?.getBoundingClientRect().top || 10000) - 150) + document.documentElement.scrollTop, behavior: "smooth" })}>
+      <p className="TextHeavy" style={{backgroundColor:'#2E3744', color:'#F1F1F1', padding:'5px', borderRadius:'4px'}}>
+                  HIGLIGHTS
+              </p>
+        </div>
+
+        <div className="NavbarItem" onClick={() => window.scrollTo({ top: ((document.getElementById('defects')?.getBoundingClientRect().top || 10000) - 150) + document.documentElement.scrollTop, behavior: "smooth" })}>
+      <p className="TextHeavy" style={{backgroundColor:'#2E3744', color:'#F1F1F1', padding:'5px', borderRadius:'4px'}}>
+                  DEFECTS
+              </p>
+        </div>
+        </>
+      }
+    
+    
+    
+  </div>
+  <Outlet/>
+
+  {!window.location.href.includes("demos") && !window.location.href.includes("blog") && <div className="Footer">
+  <p>Hayden Karp && HaydenNinja@hotmail.com && <span><a style={{color:'#F1F1F1'}} target="_blank" rel="noreferrer" href="https://github.com/ParzivalPerhaps"><u>My GitHub</u></a></span></p>
+      
+  </div>}
+  </div>
+
+);
